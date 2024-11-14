9666b8906b065685f4cdd096739ac225fb468c82
Aiard0 <aiard0yt3@gmail.com>
2024-11-14 15:23:23 -0300

    check system file using python

---
+++ chech_system.py
@@ -0,0 +1,10 @@
+import csv
+import pathlib
+from pprint import pprint
+
+path = pathlib.Path("/etc/os-release")
+with open(path) as stream:
+    reader = csv.reader(stream, delimiter="=")
+    os_release = dict(reader)
+
+pprint(os_release)
