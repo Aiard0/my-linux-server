98e8f26d34913813e8aac4033846025fa1cee45a
Aiard0 <aiard0yt3@gmail.com>
2024-11-14 15:32:34 -0300

    changing method to access linux distro

--- chech_system.py
+++ chech_system.py
@@ -1,10 +1,25 @@
-import csv
 import pathlib
-from pprint import pprint
 
+# Define o caminho do arquivo
 path = pathlib.Path("/etc/os-release")
-with open(path) as stream:
-    reader = csv.reader(stream, delimiter="=")
-    os_release = dict(reader)
 
+# Dicionário para armazenar as informações
+os_release = {}
+
+# Lê o arquivo linha por linha
+with open(path) as stream:
+    for line in stream:
+        # Remove espaços e quebras de linha
+        line = line.strip()
+        # Ignora linhas que não têm '='
+        if "=" in line:
+            key, value = line.split("=", 1)
+            # Remove aspas, se houver
+            os_release[key] = value.strip('"')
+
+# Imprime todas as informações
+from pprint import pprint
 pprint(os_release)
+
+# Imprime apenas o ID da distribuição Linux
+print("ID da distribuição Linux:", os_release.get("ID"))
